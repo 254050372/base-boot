0d1cf2af74eb9737136ace01cf4b03eb4a588ca5
chixinzei <254050372@163.com>
2018-04-13 08:26:00 +0800

    Springboot版本升级2.0 Release!

--- src/main/java/com/boot/portal/PortalApplication.java
+++ src/main/java/com/boot/portal/PortalApplication.java
@@ -1,12 +1,15 @@
 package com.boot.portal;
 
+import com.boot.portal.common.config.BaseConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootVersion;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.SpringVersion;
 
 @SpringBootApplication
--- src/main/java/com/boot/portal/controller/base/BaseController.java
+++ src/main/java/com/boot/portal/controller/base/BaseController.java
@@ -3,9 +3,12 @@ package com.boot.portal.controller.base;/**
  * @autor xbwu on 2017/8/17.
  */
 
-import com.boot.portal.common.enums.VersionEnum;
+import com.boot.portal.common.config.BaseConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -18,7 +21,7 @@ public abstract class BaseController {
     public final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     public ModelAndView getMV(String viewName){
-        return new ModelAndView(VersionEnum.getCurrentVersion()+"/"+viewName); //返回的view就是templetes下面文件的名称
+        return new ModelAndView(BaseConstants.templatesPrefix+"/"+viewName); //返回的view就是templetes下面文件的名称
     }
 
     public ModelAndView forwardController(String viewName){
